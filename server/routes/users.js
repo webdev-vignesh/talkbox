376786dd455bb221d7b6b9ea9090fdb0970edf65
vicky <vigmurug@gmail.com>
2023-02-24 23:18:23 -0500

    first commit

---
+++ server/routes/users.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+    getUser,
+    getUSerFriends,
+    addRemoveFriend,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUSerFriends);
+
+/* UPDATE */
+router.patch(":id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
